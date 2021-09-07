43e1fd058bf6fea32173d6de6a829dcee4219f28
Artem Safin <artemsafin.webdev@gmail.com>
2021-09-07 15:31:30 +0300

    Скрытие блока истории

--- src/js/revealHistoryBlock.js
+++ src/js/revealHistoryBlock.js
@@ -1,10 +1,33 @@
-export default function revealHistoryBlock() {
-    document.addEventListener('click', event => {
-        if (event.target.matches('.history__block-open-btn') || event.target.closest('.history__block-open-btn')) {
-            event.preventDefault();
-            const block = event.target.closest('.history__block');
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
-            block.classList.add('revealed');
-        }
+export default function revealHistoryBlock() {
+    const elements = Array.from(document.querySelectorAll('.history__block-open-btn'));
+
+    elements.forEach(btn => {
+        const block = btn.closest('.history__block');
+        let open = false;
+        const btnOriginalText = btn.querySelector('span').textContent;
+        const btnChangedText = btn.getAttribute('data-text');
+
+        btn.addEventListener('click', event => {
+            event.preventDefault();
+
+            if (open) {
+                block.classList.remove('revealed');
+                btn.querySelector('span').textContent = btnOriginalText;
+                btn.classList.remove('active');
+                open = false;
+                ScrollTrigger.refresh();
+            } else {
+                block.classList.add('revealed');
+                btn.querySelector('span').textContent = btnChangedText;
+                btn.classList.add('active');
+                open = true;
+                ScrollTrigger.refresh();
+            }
+        })
     })
+    
 }
